91c86038241d9af25b1312d21c9d28bba3ae3c1e
Eric <eric.tornas@gmail.com>
2024-06-27 14:33:14 -0400

    game objects

---
+++ game_objects.py
@@ -0,0 +1,28 @@
+class item:
+    def __init__(self, name: str, description: str, pow: int):
+        self.name = name
+        self.description = description
+        self.pow = pow
+    
+
+class weapon(item):
+    def __init__(self, name: str, description: str, pow: int):
+        super.__init__(name, description, pow)
+
+class character():
+    def __init__(self, name: str, type, strength, intelligence, agelity, health, luck):
+        self.name = name
+        self.type = type
+        self.strength = strength
+        self.intelligence = intelligence
+        self.agility = agelity
+        self.health = health
+        self.luck = luck
+
+         
+
+
+    def state_character(self, update_state) -> str:
+        self.body_state = update_state
+    
+
