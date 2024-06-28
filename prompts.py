480b8be01fbf031072eb456749e92cfd4ee2de34
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-06-27 23:10:28 -0400

    prompts sin errores

--- prompts.py
+++ prompts.py
@@ -3,7 +3,8 @@ ciertos niveles de acuerdo con el mundo que creaste (Puede ser un mundo de fanta
 El objetivo es que en dicho mundo se presenten progresivamente desafíos que el jugador deba superar.
 A continuación crea una historia pequeña del mundo."""
 
-PLAYERINIT_OP = f"""Dado el mundo creado: {}
+def player_init_op(world):
+    return f"""Dado el mundo creado: {world}
 Crea 3 tipos diferentes de personajes los cuales podrían interactuar con el mundo. Estos personajes deben ser simples
 y poder ir adquiriendo habilidades con el tiempo. El formato a devolver debe ser el siguiente:
 Ejemplos:
@@ -11,14 +12,15 @@ Arquero: Un aficionado al uso de armas a distancia sin mucha experiencia
 Mago: Posee el don de hacer cosas sobrenaturales pero no sabe controlarlo
 Sobreviviente: Capaz de dar un esfuerzo extra en los momentos cruciales"""
 
-def player_init_stats(player,):
+def player_init_stats(world,player,params):
     return f"""Dado esta descripción de un personaje de videojuego: {player} y
-esta descripción del mundo: {INITGAME} has una asignación a cada uno de estos parámetros: {}. Cada parámetro
+esta descripción del mundo: {world} has una asignación a cada uno de estos parámetros: {params}. Cada parámetro
 debe tener un número entre 1 y 12.
 La respuesta debe tener este formato:
 <habilidad> = <cantidad>
 Ejemplo:
 Vida = 5
+Fuerza = 7
 """
 
 def challenge(history, player, features):    
