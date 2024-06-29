49e8236856aad06e9512e2287553a993c2d0cd14
nao_lahera <lahera0103@gmail.com>
2024-06-29 02:50:06 -0400

    Function call

--- nlc.py
+++ nlc.py
@@ -6,66 +6,66 @@ client = openai.OpenAI(
     api_key = "HAicU1zXB0SL3O8NfsRDROgkPGXzQiH7jAw9SAhObuLZvbe5"
 )
 
-messages = [
-    {"role": "user", "content": "What are Nike's net income in 2022?"}
-]
-
-tools = [
-    {
-        "type": "function",
-        "function": {
-            # name of the function 
-            "name": "get_financial_data",
-            # a good, detailed description for what the function is supposed to do
-            "description": "Get financial data for a company given the metric and year.",
-            # a well defined json schema: https://json-schema.org/learn/getting-started-step-by-step#define
-            "parameters": {
-                # for OpenAI compatibility, we always declare a top level object for the parameters of the function
-                "type": "object",
-                # the properties for the object would be any arguments you want to provide to the function
-                "properties": {
-                    "metric": {
-                        # JSON Schema supports string, number, integer, object, array, boolean and null
-                        # for more information, please check out https://json-schema.org/understanding-json-schema/reference/type
-                        "type": "string",
-                        # You can restrict the space of possible values in an JSON Schema
-                        # you can check out https://json-schema.org/understanding-json-schema/reference/enum for more examples on how enum works
-                        "enum": ["net_income", "revenue", "ebdita"],
+def fc_situation_solver(content):
+    tool = [
+        {
+            "type": "function",
+            "function": {
+                "name": "fc_situation_solver_",
+                "description": "Solve a situation based on the given attributes.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "strength": {
+                            "type": "integer",
+                            "description": "Strength attribute value."
+                        },
+                        "agility": {
+                            "type": "integer",
+                            "description": "Agility attribute value."
+                        },
+                        "intelligence": {
+                            "type": "integer",
+                            "description": "Intelligence attribute value."
+                        },
+                        "health": {
+                            "type": "integer",
+                            "description": "Health attribute value."
+                        },
+                        "luck": {
+                            "type": "integer",
+                            "description": "Luck attribute value."
+                        }
                     },
-                    "financial_year": {
-                        "type": "integer", 
-                        # If the model does not understand how it is supposed to fill the field, a good description goes a long way 
-                        "description": "Year for which we want to get financial data."
-                    },
-                    "company": {
-                        "type": "string",
-                        "description": "Name of the company for which we want to get financial data."
-                    }
-                },
-                # You can specify which of the properties from above are required
-                # for more info on `required` field, please check https://json-schema.org/understanding-json-schema/reference/object#required
-                "required": ["metric", "financial_year", "company"],
-            },
-        },
-    }
-]
-
-chat_completion = client.chat.completions.create(
+                    "required": ["strength", "agility", "intelligence", "health", "luck"]
+                }
+            }
+        }
+    ]
+    
+    chat_completion = client.chat.completions.create(
     model="accounts/fireworks/models/firefunction-v2",
-    messages=messages,
-    tools=tools,
-    temperature=0.1
-)
-print(chat_completion.choices[0].message.model_dump_json(indent=4))
+    messages=[{"role": "user", "content": f"{content}"}],
+    tools=tool,
+    temperature=0.1)
+    print(chat_completion.choices[0].message.model_dump_json(indent=4))
+    
+    function_call = chat_completion.choices[0].message.tool_calls[0].function
+    local = globals()[function_call.name](**json.loads(function_call.arguments))
+    print(local)
+    return local
 
-def get_financial_data(metric: str, financial_year: int, company: str):
-    print(f"{metric=} {financial_year=} {company=}")
-    if metric == "net_income" and financial_year == 2022 and company == "Nike":
-        return {"net_income": 6_046_000_000}
-    else:
-        raise NotImplementedError()
+def fc_situation_solver_(strength, agility, intelligence, health, luck) -> dict:
+    print(f"{strength=} {agility=} {intelligence=}")
+    res = dict()
+    res['strength'] = strength
+    res['intelligence'] = intelligence
+    res['agility'] = agility
+    res['health'] = health
+    res['luck'] = luck
+    
+    return res
 
-function_call = chat_completion.choices[0].message.tool_calls[0].function
-tool_response = locals()[function_call.name](**json.loads(function_call.arguments))
-print(tool_response)
-
+content = "You are in a dangerous situation and your atributes are: strength: 0, agility: 1, intelligence: 0, health: 1, luck: 0"
+# print(fc_situation_solver(content))
+fc_situation_solver(content)
