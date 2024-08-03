f64add14f4d9245da4447ab2a3224d7479534399
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-08-03 17:34:37 -0400

    mybat

---
+++ run_leo.bat
@@ -0,0 +1,6 @@
+﻿
+
+@echo off
+
+REM Ejecutar Streamlit
+"C:/Cosas/programas nuevos/WPy64-31110/python-3.11.1.amd64/python.exe" -m streamlit run "c:/Cosas/Matcom/Cuarto Año Cibernética/Segundo Semestre/LLM/Dont_Die_LLM_Game/Home.py"
